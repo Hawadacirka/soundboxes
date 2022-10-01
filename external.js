b48ae6ce642b770c4ca83fd1f10fa6ceb7c14d78
Hawadacirka <103773398+Hawadacirka@users.noreply.github.com>
2022-10-01 19:23:17 +0100

    commit

---
+++ external.js
@@ -0,0 +1,35 @@
+document.getElementById("hiphop").onclick = function() {
+    var audio = document.getElementById("hipAudio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("jazz").onclick = function() {
+    var audio = document.getElementById("jazAudio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("rock").onclick = function() {
+    var audio = document.getElementById("rocAudio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("reggae").onclick = function() {
+    var audio = document.getElementById("regAudio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("rnb").onclick = function() {
+    var audio = document.getElementById("rnbAudio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
+
+document.getElementById("classical").onclick = function() {
+    var audio = document.getElementById("claAudio");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
